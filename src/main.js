c9e23a63d030d7640e32bdf3976c61c94e1d6249
Abdul Rehman <mehars.6925@gmail.com>
2019-10-13 10:51:12 +0500

    initialized vue-router, bootstrap-vue, vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import * as firebase from 'firebase'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID
+}
+firebase.initializeApp(config)
+
 new Vue({
+  data: {
+    title: 'CarefordCorp'
+  },
   render: h => h(App),
 }).$mount('#app')

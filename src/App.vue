6d87a7aeab1e4c0106387a1398c93a9a3bab8f20
Abdul Rehman <mehars.6925@gmail.com>
2019-10-19 16:32:23 +0500

    setup router-view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Navigation />
+    <router-view></router-view>
+    <Foot />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const Foot = () => import('./components/Foot')
+const Navigation = () => import('./components/Navigation')
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Foot,
+    Navigation
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

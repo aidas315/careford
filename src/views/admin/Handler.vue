1f2da9e174817973c4037c540129ec8f69d16e58
Abdul Rehman <mehars.6925@gmail.com>
2019-10-19 16:36:26 +0500

    router-view for authenticated routes

---
+++ src/views/admin/Handler.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        authenticated() {
+            return this.$store.getters.authenticated
+        }
+    },
+    watch: {
+        authenticated() {
+            if (!this.authenticated) {
+                this.$router.push({ name: 'home' })
+            }
+        }
+    }
+}
+</script>

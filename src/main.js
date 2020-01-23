e58d54cb9e68daa2c8dbde1b5e4689f23f5f9f95
Abdul Rehman <mehars.6925@gmail.com>
2020-01-23 14:10:54 +0500

    installed font-awesome and added methods

--- src/main.js
+++ src/main.js
@@ -4,16 +4,32 @@ import App from './App.vue'
 import { store } from './vuex'
 import VueRoutes from './routes'
 import VueRouter from 'vue-router'
+import vueHeadful from "vue-headful"
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import CKEditor from '@ckeditor/ckeditor5-vue'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+import { faPhone, faEnvelope, faArchway, faLayerGroup, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+
+library.add(
+  faPhone,
+  faEnvelope,
+  faArchway,
+  faLayerGroup,
+  faWhatsapp,
+  faChevronRight
+)
 
 Vue.use(Vuex)
 Vue.use(CKEditor)
 Vue.use(VueRouter)
 Vue.use(BootstrapVue) 
 Vue.config.productionTip = false
+Vue.component("vue-headful", vueHeadful)
+Vue.component("font-awesome-icon", FontAwesomeIcon)
 
 const router = new VueRouter({
   mode: 'history',
@@ -23,9 +39,10 @@ const router = new VueRouter({
 
 new Vue({
   data: {
-    title: 'Careford'
+    title: 'CarefordCorp'
   },
   created() {
+    this.$store.dispatch('load_banners')
     this.$store.dispatch('load_products')
     this.$store.dispatch('load_categories')
     this.$store.dispatch('check_authentication')
@@ -39,6 +56,12 @@ new Vue({
     logout() {
       this.$store.dispatch('logout')
       this.$router.push({ name: 'home' })
+    },
+    pageTitle() {
+      return " - " + this.title
+    },
+    pageDescription() {
+      return `Careford Corporation is leading Manufacturer of professional Beauty Instruments. We strive to achieve quality without compromise in delivering great products, each time, every time. Careford Corporation focuses on doing the basics right and adding value through innovation and technical excellence. We pride ourselves on our exceptional services and bringing competitive solution to your product.`;
     }
   },
   store,
